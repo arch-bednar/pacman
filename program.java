6d50622f997c9e55c5ef0d395351b61e3fde826f
Marvin <dawid.bednarczyk@protonmail.com>
2021-07-17 20:52:44 -0700

    new file

--- program.java
+++ program.java
@@ -2,5 +2,9 @@ public class program{
     public static void main(String[] args){
         System.out.println("hellow!");
         System.out.println("xDDDDD");
+        writeln("siemasz");
+    }
+    public static void writeln(string str){
+        System.out.println(str);
     }
 }
